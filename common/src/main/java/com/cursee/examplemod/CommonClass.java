bffd36c9a0c2fbdd7b8d723b5ac456b6076ccc63
jason13official <135896906+jason13official@users.noreply.github.com>
2024-06-29 12:33:27 -0400

    Update CommonClass.java

--- common/src/main/java/com/cursee/examplemod/CommonClass.java
+++ common/src/main/java/com/cursee/examplemod/CommonClass.java
@@ -4,8 +4,5 @@ import com.cursee.monolib.core.sailing.Sailing;
 
 public class CommonClass {
 
-    public static void init() {
-
-        Sailing.register(Constants.MOD_NAME, Constants.MOD_ID, Constants.MOD_VERSION, Constants.MC_VERSION_RAW, Constants.PUBLISHER_AUTHOR, Constants.PRIMARY_CURSEFORGE_MODRINTH);
-    }
+    public static void init() {}
 }
