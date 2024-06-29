3c19f83d878601a39e2d66db325e374d8554eb1f
jason13official <135896906+jason13official@users.noreply.github.com>
2024-06-29 12:29:12 -0400

    Update Constants.java

--- common/src/main/java/com/cursee/examplemod/Constants.java
+++ common/src/main/java/com/cursee/examplemod/Constants.java
@@ -7,9 +7,9 @@ import oshi.util.tuples.Triplet;
 
 public class Constants {
 
-	public static final String MOD_ID = "examplemod";
-	public static final String MOD_NAME = "ExampleMod";
-	public static final String MOD_VERSION = "1.0.0";
+	public static final String MOD_ID = "disenchanting_table";
+	public static final String MOD_NAME = "Dis-Enchanting Table";
+	public static final String MOD_VERSION = "3.0.0";
 	public static final String MC_VERSION_RAW = "[1.20.6]";
 	public static final Pair<String, String> PUBLISHER_AUTHOR = new Pair<String, String>("Lupin", "Jason13");
 	public static final Triplet<String, String, String> PRIMARY_CURSEFORGE_MODRINTH = new Triplet<String, String, String>("https://www.curseforge.com/members/lupin/projects", "https://www.curseforge.com/members/lupin/projects", "https://modrinth.com/user/Lupin/mods");
