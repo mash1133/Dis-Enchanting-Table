e5b5b08cb9b97e87821a9d4a0064db2ad94778c5
jason13official <jason13official@gmail.com>
2024-07-05 16:14:47 -0400

    bump version, make block mineable

--- common/src/main/java/com/cursee/disenchanting_table/Constants.java
+++ common/src/main/java/com/cursee/disenchanting_table/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
 
 	public static final String MOD_ID = "disenchanting_table";
 	public static final String MOD_NAME = "Dis-Enchanting Table";
-	public static final String MOD_VERSION = "3.0.0";
+	public static final String MOD_VERSION = "3.0.1";
 	public static final String MC_VERSION_RAW = "[1.20.1]";
 	public static final Pair<String, String> PUBLISHER_AUTHOR = new Pair<String, String>("Lupin", "Jason13");
 	public static final Triplet<String, String, String> PRIMARY_CURSEFORGE_MODRINTH = new Triplet<String, String, String>("https://www.curseforge.com/members/lupin/projects", "https://www.curseforge.com/members/lupin/projects", "https://modrinth.com/user/Lupin/mods");
